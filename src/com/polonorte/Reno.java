d35072ff2778c90aab956e7cc735c0a95d13d6b2
diegoProFP <diego.di1@educa.madrid.org>
2023-03-03 09:38:51 +0100

    Se eliminan comentarios Javadoc... para ponerlos despues :)

--- src/com/polonorte/Reno.java
+++ src/com/polonorte/Reno.java
@@ -1,12 +1,6 @@
 package com.polonorte;
 
-/**
- * Clase que representa un reno
- * (más comentarios)
- * @author Diego
- *
- * @version 1.0
- */
+
 public class Reno {
 
     private String nombre;
@@ -14,23 +8,14 @@ public class Reno {
 
     private int numCuernos;
     private int numCollares;
-    /**
-     * Metodo para que un reno se ponga a volar
-     * @param direccion La direccion a la que tiene que ir el reno
-     * @return El grito de guerra que devuelve el reno
-     */
+   
     public String volar(String direccion){
         System.out.println("Reno.volar");
 
-        System.out.println("Reno volando");//comment
         return "mmmmmiiiiii chus chus peeeeerrrr";
     }
 
-    /**
-     * Metodo que hace correr a este reno hacia una direccion
-     * @deprecated Deprecado desde la version 0.5, mejor usar {@link #volar(String)}
-     * @param direccion Direccion a la que tiene que ir el reno
-     */
+    
     public void correr(String direccion){
         //TODO terminar
     }
--- src/com/polonorte/Trineo.java
+++ src/com/polonorte/Trineo.java
@@ -2,11 +2,7 @@ package com.polonorte;
 
 import java.util.List;
 
-/**
- * Clase que representa un <i>trineo de navidad</i>
- * @author Diego
- * @version 1.0
- */
+
 public class Trineo {
 
     private int numAsientos;
@@ -14,47 +10,28 @@ public class Trineo {
     private int volumenRegalos;
     private List<String> bolsaRegalos;
     public static void main(String[] args) {
-        String paco= "aaaa";
+     
         System.out.println("Clase Trineo.main");
         Reno rudolf = new Reno();
-        rudolf.volar("pallá");
+        rudolf.volar("vamos!");
     }
 
-    /**
-     * @see com.polonorte.Reno
-     */
+  
     private List<Reno> renos;
 
-    /**
-     * Mete un regalo dentro de la bolsa
-     * @param regalo El regalo a meter en la lista
-     * @return El resultado de añadir el regalo a la lista
-     * @since 1.0
-     */
+   
     public boolean meterRegalo(String regalo){
         return this.bolsaRegalos.add(regalo);
     }
 
-    /**
-     * Incluye un reno en la lista de renos del trineo
-     * @param uno  El reno a incluir
-     * @return El resultado de añadir el reno a la lista
-     * @see com.polonorte.Reno
-     */
+   
     public boolean incluirReno(Reno uno){
         return this.renos.add(uno);
 
     }
 
 
-    /**
-     * Metodo para calcular si un niño/a ha sido bueno, por lo tanto merece o no regalo.
-     * @param nombre El nombre del niño/a
-     * @param numeroPeleas La cantidad de peleas en las que ha estado involucrado
-     * @param vecesGracias El numero de veces que ha dicho <i>gracias</i>
-     * @param vecesPorFavor El numero de veces que ha dicho <i>por favor</i>
-     * @return True si merece regalo, false en caso contrario
-     */
+   
     public boolean mereceRegalo(String nombre, int numeroPeleas, int vecesGracias, int vecesPorFavor) {
     	
 	    boolean haSidoBueno = false;
