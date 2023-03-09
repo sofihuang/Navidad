6359ba5c4908b5a6a9d12e167fda2f0f82d63f45
diegoProFP <diego.di1@educa.madrid.org>
2023-03-09 22:40:36 +0100

    Se añade la clase SantaClaus

---
+++ src/com/polonorte/SantaClaus.java
@@ -0,0 +1,71 @@
+package com.polonorte;
+
+public class SantaClaus {
+
+	private static final String CARACTER_AMIGABLE = "AMIGABLE";
+	private static final String CARACTER_CABREADO = "CABREADO";
+	private static final String CARACTER_FURIOSO = "FURIOSO";
+
+	// Los kilos de peso de SantaClaus
+	private int kilosPeso;
+
+	// Si está trabajando ya o no
+	private boolean trabajando;
+
+	private String caracterActual;
+
+	public SantaClaus() {
+		this.trabajando = false;
+		this.caracterActual = CARACTER_AMIGABLE;
+	}
+
+	public String generarSaludoSegunCaracter(String caracter) {
+
+		String saludo;
+		switch (caracter) {
+		case CARACTER_AMIGABLE: 
+			saludo = "hohoho, feliz navidad pequeños!";
+			break;
+		case CARACTER_CABREADO: 
+			saludo = "¿otra vez pidiendo juguetes? ¡Me tenéis harto!";
+			break;
+		case CARACTER_FURIOSO: 
+			saludo = "ODIO LA NAVIDAAAAADD!! DEJAD DE ESCRIBIRME!";
+			break;
+		default:
+			saludo = "hoho? otra vez?";
+
+		}
+		return saludo;
+	}
+	
+	//Este se tiene que considerar obsoleto, y referenciar al metodo generarSaludoSegunCaracter
+	public String saludar() {
+		return "hohoho";
+	}
+
+	public int getKilosPeso() {
+		return kilosPeso;
+	}
+
+	public void setKilosPeso(int kilosPeso) {
+		this.kilosPeso = kilosPeso;
+	}
+
+	public boolean isTrabajando() {
+		return trabajando;
+	}
+
+	public void setTrabajando(boolean trabajando) {
+		this.trabajando = trabajando;
+	}
+
+	public String getCaracterActual() {
+		return caracterActual;
+	}
+
+	public void setCaracterActual(String caracterActual) {
+		this.caracterActual = caracterActual;
+	}
+
+}
