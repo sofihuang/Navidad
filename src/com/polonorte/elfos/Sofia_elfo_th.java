737b35a793010c5f73adec714e6ef2d17c0ce755
sofihuang <distance75432@gmail.com>
2023-03-13 09:53:49 +0100

    Sofia_elfo_th

---
+++ src/com/polonorte/elfos/Sofia_elfo_th.java
@@ -0,0 +1,11 @@
+package com.polonorte.elfos;
+
+public class Sofia_elfo_th extends ElfoGenerico {
+
+	protected Sofia_elfo_th(String nombre, Enum seccionLaboral) {
+		super("sofia", SeccionLaboralElfo.VIDEOCONSOLAS);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
